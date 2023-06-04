cb66c4f924e0bfd9f599760a026a389637992d40
Marcos Mollerach <mmollerach98@hotmail.com>
2023-06-04 16:07:07 -0300

    layout basico

---
+++ M4U1/layout_basico/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Body = () => {
+  return (
+    <section>
+      <h2>Cuerpo</h2>
+      <p>Contenido del cuerpo</p>
+    </section>
+  );
+};
+
+export default Body;
---
+++ M4U1/layout_basico/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Encabezado</h1>
+    </header>
+  );
+};
+
+export default Header;
