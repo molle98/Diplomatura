32df904b5d5700320baf3b664d79f0da780a0cfb
Marcos Mollerach <mmollerach98@hotmail.com>
2023-08-21 14:58:08 -0300

    indentation fix

--- ProyectoFinal/frontend/src/pages/ContactoPage.js
+++ ProyectoFinal/frontend/src/pages/ContactoPage.js
@@ -2,63 +2,92 @@ import { useState } from "react";
 import axios from "axios";
 
 const ContactoPage = (props) => {
+  const initialForm = {
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  };
 
-    const initialForm = {
-      name: '',
-      email: '',
-      phone: '',
-      message: ''
+  const [sending, setSending] = useState(false);
+  const [msg, setMsg] = useState("");
+  const [formData, setFormData] = useState(initialForm);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((oldData) => ({
+      ...oldData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMsg("");
+    setSending(true);
+    const response = await axios.post(
+      "http://localhost:3000/contacto",
+      formData
+    );
+    setSending(false);
+    setMsg(response.data.message);
+    if (response.data.error === false) {
+      setFormData(initialForm);
     }
-
-    const [sending, setSending] = useState(false);
-    const [msg, setMsg] = useState('');
-    const [formData, setFormData] = useState(initialForm);
-
-    const handleChange = e => {
-      const {name, value} = e.target;
-      setFormData(oldData => ({
-        ...oldData,
-        [name]: value
-      }));
-    }
-
-    const handleSubmit = async e => {
-      e.preventDefault();
-      setMsg('');
-      setSending(true)
-      const response = await axios.post('http://localhost:3000/contacto', formData);
-      setSending(false);
-      setMsg(response.data.message);
-      if (response.data.error === false){
-        setFormData(initialForm)
-      }
-    }
-
-
-    
-
-
+  };
 
   return (
     <main className="holder contacto">
       <div>
         <h2>Contacto</h2>
-        <form action="http://localhost:3000/contacto" method="post" onSubmit={handleSubmit}>
+        <form
+          action="http://localhost:3000/contacto"
+          method="post"
+          onSubmit={handleSubmit}
+        >
           <div class="form-group">
             <label for="name">Nombre:</label>
-            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange}required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div class="form-group">
             <label for="email">Telefono:</label>
-            <input type="phone" id="phone" name="phone" value={formData.phone} onChange={handleChange}required />
+            <input
+              type="phone"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+              required
+            />
           </div>
           <div class="form-group">
             <label for="message">Mensaje:</label>
-            <textarea id="message" name="message" rows="5" value={formData.message} onChange={handleChange} required></textarea>
+            <textarea
+              id="message"
+              name="message"
+              rows="5"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            ></textarea>
           </div>
           <div class="form-group">
             <input type="submit" value="Enviar" />
